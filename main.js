c311ea8ebb3adf5899442673b23de690ac3ceb7c
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-15 15:36:03 +0100

    using for

--- main.js
+++ main.js
@@ -10,5 +10,13 @@ document.querySelector("#app").innerHTML = /*html*/ `
 	<button class="btn-danger">Delete</button>
 	<button class="btn-warning">Backup</button>
 
+	<hr>
+
+	<p class="box-1"></p>
+	<p class="box-2"></p>
+	<p class="box-3"></p>
+	<p class="box-4"></p>
+	<p class="box-5"></p>
+
 </section>
 `;
