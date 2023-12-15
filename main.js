3084c98c2785ce0f99a7c184cbf712ef04c241ee
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-15 15:02:13 +0100

    plain buttons in

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-import './style.scss'
+import "./style.scss";
 
-document.querySelector('#app').innerHTML = /*html*/ `
+document.querySelector("#app").innerHTML = /*html*/ `
 <section class="main">
-  <h1>Sass Site</h1>
-  <p>Welcome to this site.</p>
-</section>
-`
+	<h1>Sass Site</h1>
+	<p>Welcome to this site.</p>
 
+	<button class="btn-primary">Get Info</button>
+	<button class="btn-danger">Delete</button>
+	<button class="btn-warning">Backup</button>
+
+</section>
+`;
